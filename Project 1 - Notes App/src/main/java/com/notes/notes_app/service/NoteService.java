d493341d10819be6b12802fb053bc46a39688fd5
Megabear137 <Megabear137>
2026-06-06 21:56:33 -0400

    Complete NoteService

--- Project 1 - Notes App/src/main/java/com/notes/notes_app/service/NoteService.java
+++ Project 1 - Notes App/src/main/java/com/notes/notes_app/service/NoteService.java
@@ -2,6 +2,7 @@ package com.notes.notes_app.service;
 
 import com.notes.notes_app.dto.NoteCreateRequest;
 import com.notes.notes_app.dto.NoteResponse;
+import com.notes.notes_app.dto.NoteUpdateRequest;
 import com.notes.notes_app.entity.Note;
 import com.notes.notes_app.entity.User;
 import com.notes.notes_app.repository.NoteRepository;
@@ -24,38 +25,53 @@ public class NoteService {
         this.userRepository = userRepository;
     }
 
-    public List<NoteResponse> list(Long owner_id) {
+    public List<NoteResponse> list(Long ownerId) {
         return noteRepository
-                .findByOwnerId(owner_id)
+                .findByOwnerId(ownerId)
                 .stream()
                 .map(NoteResponse::from)
                 .toList();
     }
 
-    public NoteResponse get(Long owner_id, Long id) {
+    public NoteResponse get(Long ownerId, Long id) {
         Optional<Note> note = noteRepository.findById(id);
-        if(note.isPresent() && note.get().getOwner().getId().equals(owner_id)) {
+        if(note.isPresent() && note.get().getOwner().getId().equals(ownerId)) {
             return NoteResponse.from(note.get());
         }
         throw new RuntimeException("Note not Found");
     }
 
-    public void delete(Long id, Long owner_id) {
+    public void delete(Long id, Long ownerId) {
         Optional<Note> note = noteRepository.findById(id);
-        if(note.isPresent() && note.get().getOwner().getId().equals(owner_id)) {
+        if(note.isPresent() && note.get().getOwner().getId().equals(ownerId)) {
             noteRepository.delete(note.get());
         }
+        else throw new RuntimeException("Note not Found");
     }
 
-    public NoteResponse create(Long owner_id, NoteCreateRequest request) {
-        User owner = userRepository.findById(owner_id)
+    public NoteResponse update(Long ownerId, Long id, NoteUpdateRequest request) {
+        Optional<Note> note = noteRepository.findById(id);
+        if(note.isPresent() && note.get().getOwner().getId().equals(ownerId)) {
+            Note foundNote = note.get();
+
+            foundNote.setTitle(request.title());
+            foundNote.setBody(request.body());
+            foundNote.setUpdatedAt(Instant.now());
+
+            Note saved = noteRepository.save(foundNote);
+            return NoteResponse.from(saved);
+        }
+        else throw new RuntimeException("Note not Found");
+    }
+
+    public NoteResponse create(Long ownerId, NoteCreateRequest request) {
+        User owner = userRepository.findById(ownerId)
                 .orElseThrow(() -> new RuntimeException("User not found"));
 
         Note note = new Note();
         note.setOwner(owner);
         note.setTitle(request.title());
         note.setBody(request.body());
-        note.setCreatedAt(Instant.now());
 
         Note saved = noteRepository.save(note);
         return NoteResponse.from(saved);
